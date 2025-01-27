7df9c0e31337ddba05579dd90b6c2541f1a2e09b
Angeluss2023 <bryamfer200@gmail.com>
2025-01-26 21:56:40 -0500

    Primer commit - Frontend

--- src/app/services/tiktok.service.ts
+++ src/app/services/tiktok.service.ts
@@ -3,13 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root', // Asegura que el servicio esté disponible globalmente
+  providedIn: 'root', // Servicio disponible globalmente
 })
 export class TiktokService {
   private baseUrl: string = 'http://127.0.0.1:5000'; // URL del servidor Flask
 
   constructor(private http: HttpClient) {}
 
+  ejecutarExtraccion(): Observable<any> {
+    return this.http.post('http://127.0.0.1:5000/ejecutar_extraccion', {});
+  }
+  
+  // Método para obtener los comentarios
   getComentarios(): Observable<any> {
     return this.http.get(`${this.baseUrl}/obtener_comentarios`);
   }
